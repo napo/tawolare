a009764505a94c10ad7458a37c805b5a6815937b
Maurizio Napolitano <napo@fbk.eu>
2018-03-13 15:49:38 +0100

    setup cambiato in funzione dei nuovi dati catastali

--- setup_catasto.sh
+++ setup_catasto.sh
@@ -1,22 +1,21 @@
-wget http://www.territorio.provincia.tn.it/geodati/798_Limiti_di_comune_catastale_07_01_2016.zip
-unzip -o 798_Limiti_di_comune_catastale_07_01_2016.zip
+#scarico limiti catastali
+wget http://www.territorio.provincia.tn.it/geodati/798_Metadato_12_02_2018.zip -O limiti_comune_catastale.zip
+unzip -o limiti_comune_catastale.zip
 spatialite_tool -i -shp ammcat -d catasto.sqlite -t ammcat -c CP1252 -s 3044
-wget http://www.territorio.provincia.tn.it/geodati/867_Limite_di_Comune_Amministrativo_07_01_2016.zip
-unzip -o 867_Limite_di_Comune_Amministrativo_07_01_2016.zip
+wget http://www.territorio.provincia.tn.it/geodati/867_Limite_di_Comune_Amministrativo_12_02_2018.zip -O limiti_amministrativi.zip
+unzip -o limiti_amministrativi.zip
 spatialite_tool -i -shp ammcom -d catasto.sqlite -t ammcom -c CP1252 -s 3044
 mkdir data
 cd data
-wget http://www.territorio.provincia.tn.it/geodati/catasto/1598/1598.zip
-unzip -o 1598.zip
+wget http://www.territorio.provincia.tn.it/geodati/catasto/2039_catasto_shp.zip -O catasto.zip
+unzip -o catasto.zip
+rm catasto.zip
 for i in `ls *.zip`
 do
-unzip -o $i
-done
-for i in `ls *.dxf`; 
-do 
-name=`basename $i .dxf`
-cp ../ammcom.prj $name.prj
-spatialite_tool -i -shp $name -d ../catasto.sqlite -t $name -c CP1252 -s 3044
+name="`basename $i _shp.zip`_vl_parcel_poly"
+unzip -o $i $name*
+nname="`basename $i _shp.zip`"
+spatialite_tool -i -shp $name -d ../catasto.sqlite -t $nname -c CP1252 -s 3044
 done
 cd ..
 rm -fr data
